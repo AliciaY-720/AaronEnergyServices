0dad862b2891a3ad203f16e41827c7eb3a93b81a
Beixuan Yang <yangbeixuan@gmail.com>
2024-03-26 21:30:31 -0400

    edit footer

--- aarone_backend/projects/views.py
+++ aarone_backend/projects/views.py
@@ -23,6 +23,25 @@ class ImageDetailView(generics.RetrieveAPIView):
     serializer_class = ImageSerializer
 
 
+class ProjectImagesView(generics.ListAPIView):
+    serializer_class = ImageSerializer
+    # def get_queryset(self):
+    #     project_id = self.kwargs['project_id']
+    #     return Image.objects.filter(project_id=project_id)
+    def get_queryset(self):
+        # project_id = self.request.query_params.get('project_id')
+        # print(project_id)
+        # if project_id is not None:
+        #     return Image.objects.filter(project__id=project_id)
+        # else:
+        #     return Image.objects.none()
+        project_id = self.kwargs.get('pk')
+        if project_id is not None:
+            return Image.objects.filter(project__id=project_id)
+        else:
+            return Image.objects.none()
+
+
 class TagListView(generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
