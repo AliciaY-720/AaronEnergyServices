7330de0552e188277bb2203fe3da0c59c1d171a4
Beixuan Yang <yangbeixuan@gmail.com>
2024-03-30 16:35:47 -0400

    Add unit tests for the service model

--- aarone_backend/projects/tests.py
+++ aarone_backend/projects/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from .models import Project, Image, Tag, Service
 from django.urls import reverse
+from .views import ServiceDetailView, ServiceListView
 
 PATH = "/api"
 class ProjectModelTestCase(TestCase):
@@ -66,3 +67,30 @@ class TagViewsTestCase(TestCase):
         tag = Tag.objects.create(name="Test Tag")
         response = self.client.get(PATH + f'/tags/{tag.pk}/')
         self.assertEqual(response.status_code, 200)
+
+class ServiceListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        Service.objects.create(name='Service 1', description='Description 1')
+        Service.objects.create(name='Service 2', description='Description 2')
+
+    def test_service_list_view(self):
+        request = RequestFactory().get('/services/')
+        view = ServiceListView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)  # Assuming there are two services
+
+class ServiceDetailViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        cls.service = Service.objects.create(name='Service 1', description='Description 1')
+
+    def test_service_detail_view(self):
+        request = RequestFactory().get('/services/1/')  # Assuming the service ID is 1
+        view = ServiceDetailView.as_view()
+        response = view(request, pk=self.service.pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'Service 1')
