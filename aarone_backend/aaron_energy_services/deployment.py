386f7be819f3a128ca859ab34aa5d591af1830da
Beixuan Yang <yangbeixuan@gmail.com>
2024-04-08 01:02:36 -0400

    modify deployment settings

--- aarone_backend/aaron_energy_services/deployment.py
+++ aarone_backend/aaron_energy_services/deployment.py
@@ -3,8 +3,8 @@ from .settings import BASE_DIR
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME')]
 DEBUG = False
 
 MIDDLEWARE = [
